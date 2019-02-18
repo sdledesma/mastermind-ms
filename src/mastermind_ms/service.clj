291c8ea4ef495136f733123bbc276c7d98c99acb
Santiago de Ledesma <sdledesma@gmail.com>
2019-02-18 09:44:37 -0600

    Initial commit

---
+++ src/mastermind_ms/service.clj
@@ -0,0 +1,100 @@
+(ns mastermind-ms.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [ring.util.response :as ring-resp]
+            [mastermind.core :as mm]
+            [mastermind.json :as mmj]))
+
+(defn about-page
+  [request]
+  (ring-resp/response (format "Clojure %s - served from %s"
+                              (clojure-version)
+                              (route/url-for ::about-page))))
+(defn guess
+  [request]
+  (let [req-data (:json-params request)
+        secret   (mmj/decode (:key req-data))
+        guess    (apply vector (map keyword (:guess req-data)))]
+    (println guess)
+    (http/json-response (mm/check-move-secret secret guess))))
+
+(defn decode
+  [request]
+  (let [req-data (:json-params request)
+        secret   (mmj/decode (:key req-data))]
+    (http/json-response secret)))
+
+(defn home-page
+  [request]
+  (ring-resp/response "Hello Mastermind player!"))
+
+(defn get-random-combination
+  [request]
+  (http/json-response {:key (mmj/encode (mm/generate-random-combination))}))
+
+;; Defines "/" and "/about" routes with their associated :get handlers.
+;; The interceptors defined after the verb map (e.g., {:get home-page}
+;; apply to / and its children (/about).
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+;; Tabular routes
+(def routes #{["/" :get (conj common-interceptors `home-page)]
+              ["/random" :get (conj common-interceptors `get-random-combination)]
+              ["/about" :get (conj common-interceptors `about-page)]
+              ["/guess" :post (conj common-interceptors `guess)]
+              ["/decode" :post (conj common-interceptors `decode)]})
+
+;; Map-based routes
+;(def routes `{"/" {:interceptors [(body-params/body-params) http/html-body]
+;                   :get home-page
+;                   "/about" {:get about-page}}})
+
+;; Terse/Vector-based routes
+;(def routes
+;  `[[["/" {:get home-page}
+;      ^:interceptors [(body-params/body-params) http/html-body]
+;      ["/about" {:get about-page}]]]])
+
+
+;; Consumed by mastermind-ms.server/create-server
+;; See http/default-interceptors for additional options you can configure
+(def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; ::http/interceptors []
+              ::http/routes routes
+
+              ;; Uncomment next line to enable CORS support, add
+              ;; string(s) specifying scheme, host and port for
+              ;; allowed source(s):
+              ;;
+              ;; "http://localhost:8080"
+              ;;
+              ;;::http/allowed-origins ["scheme://host:port"]
+
+              ;; Tune the Secure Headers
+              ;; and specifically the Content Security Policy appropriate to your service/application
+              ;; For more information, see: https://content-security-policy.com/
+              ;;   See also: https://github.com/pedestal/pedestal/issues/499
+              ;;::http/secure-headers {:content-security-policy-settings {:object-src "'none'"
+              ;;                                                          :script-src "'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:"
+              ;;                                                          :frame-ancestors "'none'"}}
+
+              ;; Root for resource interceptor that is available by default.
+              ::http/resource-path "/public"
+
+              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
+              ;;  This can also be your own chain provider/server-fn -- http://pedestal.io/reference/architecture-overview#_chain_provider
+              ::http/type :jetty
+              ;;::http/host "localhost"
+              ::http/port (Integer. (or (System/getenv "PORT") 5000))
+              ;; Options to pass to the container (Jetty)
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        ;:keystore "test/hp/keystore.jks"
+                                        ;:key-password "password"
+                                        ;:ssl-port 8443
+                                        :ssl? false}})
